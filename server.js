15f7e26fc91ae563bf5a2ef0990d3be552017ec7
Nhan Nguyen <nathan13574@gmail.com>
2020-06-15 17:38:41 -0500

    m

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ if (process.env.NODE_ENV !== 'production') {
     dotenv.config()
 }
 
+var redirect_uri = "https://heroku-spotify-project.herokuapp.com/callback"
+
 const app = express();
 
 app.use(cors());
@@ -27,7 +29,7 @@ app.get('/login', (req, res) => {
             response_type: 'code',
             client_id: process.env.SPOTIFY_CLIENT_ID,
             scope: 'user-read-recently-played user-read-private user-read-email user-top-read playlist-modify-public playlist-modify-private playlist-read-private playlist-read-collaborative user-follow-read',
-            redirect_uri: process.env.REDIRECT_URI
+            redirect_uri: redirect_uri
         }));
 });
 
@@ -38,7 +40,7 @@ app.get('/callback', (req, res) => {
         url: 'https://accounts.spotify.com/api/token',
         form: {
             code: code,
-            redirect_uri: process.env.REDIRECT_URI,
+            redirect_uri: redirect_uri,
             grant_type: 'authorization_code'
         },
         headers: {
