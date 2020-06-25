487461af1d1a317c1f75488227952d0975d20e94
Nhan Nguyen <nathan13574@gmail.com>
2020-06-25 17:26:23 -0500

    spotify

--- spotify/src/environments/environment.prod.ts
+++ spotify/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  loginUrl: "https://localhost:8000/login",
+  loginUrl: "https://heroku-spotify-project.herokuapp.com/login",
   spotifyApi: {
     host: "https://api.spotify.com/v1/",
   },
--- spotify/src/environments/environment.ts
+++ spotify/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  loginUrl: "http://localhost:8000/login",
+  loginUrl: "http://heroku-spotify-project.herokuapp.com/login",
   spotifyApi: {
     host: "https://api.spotify.com/v1/",
   },
